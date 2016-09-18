e303384fd6cdde286a07c0d0ec97cb224cd2bb0c
Mikhail Lapshin <lapshin.michail@gmail.com>
2016-09-18 21:25:45 +0300

    Вынес работу с миграциями в отдельную модель

--- server/controllers/TablesController.php
+++ server/controllers/TablesController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 use yii\web\Controller;
+use app\models\Migration;
 use yii;
 
 class TablesController extends Controller
@@ -15,83 +16,50 @@ class TablesController extends Controller
     public function actionGet($name = false)
     {
         if(!$name)
-            throw new yii\base\Exception('not fill name field');
-        $sql = 'DESCRIBE ' . $name;
+            throw new yii\base\Exception("not fill name field");
+        $sql = "DESCRIBE " . $name;
         $m = Yii::$app->db->createCommand($sql)->queryAll();
         print_r(json_encode($m));
     }
 
+
+    public function actionTest()
+    {
+       print_r(Migration::executeCommand("migrate"));
+    }
+
     public function actionCreate($name = false)
-    {
-        $tables = $this->getTables();
-        foreach ($tables as $row){
-            if($row['table'] == $name){
-                throw new yii\base\Exception('db name is busy');
-            }
-        }
-        print_r($this->createOrDropTable($name));
-    }
-
-    public function actionDrop($name = false)
-    {
-        print_r($this->createOrDropTable($name, true));
-    }
-
-    private function getTables(){
-        $sql = 'SHOW TABLES';
-        $query = Yii::$app->db->createCommand($sql)->queryColumn();
-        $response = [];
-        foreach ($query as $i => $item)
-        {
-            $response[$i]['table'] = $item;
-        }
-        return $response;
-    }
-
-    private function createOrDropTable($name = false, $drop = false)
     {
         if(!$name)
             throw new yii\base\Exception('not fill name field');
 
-        $data = array(
-            'jsonrpc' => '2.0',
-            'method'  => 'yii'
-        );
-
-        if($drop)
-            $data['params'] =  ["migrate/create drop_" . $name . "_table"];
-        else
-            $data['params'] =  ["migrate/create create_" . $name . "_table"];
-
-        $options = array(
-            'http' => array(
-                'method'  => 'POST',
-                'content' => json_encode( $data ),
-                'header'=>  "Content-Type: application/json\r\n" .
-                    "Accept: application/json\r\n"
-            )
-        );
-        $context  = stream_context_create( $options );
-        file_get_contents('http://' . $_SERVER["HTTP_HOST"] . '/webshell/default/rpc', false, $context );
-
-        $data = array(
-            'jsonrpc' => '2.0',
-            'method'  => 'yii',
-            'params'  =>  ["migrate"]
-        );
-        $options = array(
-            'http' => array(
-                'method'  => 'POST',
-                'content' => json_encode( $data ),
-                'header'=>  "Content-Type: application/json\r\n" .
-                    "Accept: application/json\r\n"
-            )
-        );
-        $context  = stream_context_create( $options );
-        $result = file_get_contents('http://' . $_SERVER["HTTP_HOST"] . '/webshell/default/rpc', false, $context );
-
-        $response = json_decode( $result );
-        return $response;
+        $tables = $this->getTables();
+        foreach ($tables as $row){
+            if($row['table'] == $name){
+                throw new yii\base\Exception("db name is busy");
+            }
+        }
+        //print_r($this->createOrDropTable($name));
+        print_r(Migration::executeCommand("migrate/create create_" . $name . "_table"));
+        print_r(Migration::executeCommand("migrate"));
     }
 
+    public function actionDrop($name = false)
+    {
+        if(!$name)
+            throw new yii\base\Exception('not fill name field');
+        print_r(Migration::executeCommand("migrate/create drop_" . $name . "_table"));
+        print_r(Migration::executeCommand("migrate"));
+    }
+
+    private function getTables(){
+        $sql = "SHOW TABLES";
+        $query = Yii::$app->db->createCommand($sql)->queryColumn();
+        $response = [];
+        foreach ($query as $i => $item)
+        {
+            $response[$i]["table"] = $item;
+        }
+        return $response;
+    }
 }
---
+++ server/models/Migration.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\models;
+
+use Yii;
+
+/**
+ * Created by PhpStorm.
+ * User: lapsh
+ * Date: 16.09.2016
+ * Time: 20:36
+ */
+class Migration
+{
+    public static function executeCommand($cmd = null)
+    {
+        $data = array(
+            'jsonrpc' => '2.0',
+            'method'  => 'yii'
+        );
+
+        $data['params'] =  [$cmd];
+
+        $options = array(
+            'http' => array(
+                'method'  => 'POST',
+                'content' => json_encode( $data ),
+                'header'=>  "Content-Type: application/json\r\n" .
+                    "Accept: application/json\r\n"
+            )
+        );
+        $context  = stream_context_create( $options );
+
+        $result = file_get_contents('http://' . $_SERVER["HTTP_HOST"] . '/webshell/default/rpc', false, $context );
+
+        $response = json_decode( $result );
+        return $response;
+    }
+}
